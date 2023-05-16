09435f9ea2dbd1167db1d604c0a91223b5fb8079
Matúš Néma <matusnema@gmail.com>
2023-05-17 01:04:24 +0200

    Created Student Middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,5 +65,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'teacher' => \App\Http\Middleware\CheckTeacherRole::class,
+        'student' => \App\Http\Middleware\CheckStudentRole::class,
     ];
 }
