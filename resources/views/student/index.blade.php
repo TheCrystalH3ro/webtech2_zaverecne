4a4a17b22b1f260a214321f547df09f2ee77fe37
Matúš Néma <matusnema@gmail.com>
2023-05-22 22:24:44 +0200

    Created DataTable

--- resources/views/student/index.blade.php
+++ resources/views/student/index.blade.php
@@ -1,13 +1,13 @@
 <h1>{{ __('Students') }}</h1>
-<table>
+<table id="students-table" class="dataTable">
     <thead>
         <tr>
-            <th>{{ __('ID') }}</th>
-            <th>{{ __('Firstname') }}</th>
-            <th>{{ __('Lastname') }}</th>
-            <th>{{ __('Num. of submitted problems') }}</th>
-            <th>{{ __('Num. of handed in problems') }}</th>
-            <th>{{ __('Points') }}</th>
+            <th data-col="0">{{ __('ID') }}</th>
+            <th data-col="1">{{ __('Firstname') }}</th>
+            <th data-col="2">{{ __('Lastname') }}</th>
+            <th data-col="3">{{ __('Num. of submitted problems') }}</th>
+            <th data-col="4">{{ __('Num. of handed in problems') }}</th>
+            <th data-col="5">{{ __('Points') }}</th>
         </tr>
     </thead>
     <tbody>
@@ -23,3 +23,19 @@
         @endforeach
     </tbody>
 </table>
+
+<script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://cdn.datatables.net/1.10.25/css/jquery.dataTables.min.css">
+<script src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
+
+<script>
+    $(document).ready(function() {
+        $('#students-table').DataTable({
+            columnDefs: [
+                { targets: [3], orderData: [3, 2] },
+                { targets: [4], orderData: [4, 2] },
+                { targets: [5], orderData: [5, 2] },
+            ]
+        });
+    });
+</script>
