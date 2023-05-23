1734a343f11b4c367118af7982bb1e0cc76d1007
Kristína Leitmanová <kristina.leitmanova@gmail.com>
2023-05-24 01:16:40 +0200

    Images

--- resources/views/images/index.blade.php
+++ resources/views/images/index.blade.php
@@ -1,15 +1,36 @@
-<a href="{{ route('upload.images.form') }}">{{ __('Upload new image') }}</a>
+@extends('layouts.app')
 
-@foreach ($images as $image)
-    <div>
-        <h3>
-            {{ $image->name }}
-        </h3>
-        <form action="{{ route('images.destroy', ["imageName" => $image->base]) }}" method="POST">
+@section('content')
+
+    <div class="content min-h-screen w-100">
+    
+        <form action="{{ route('upload.images') }}" method="post" enctype="multipart/form-data">
             @csrf
-            @method('DELETE')
-            <button type="submit">{{ __('Delete image') }}</button>
+
+            <div class="d-flex gap-2 mb-3"> 
+                <input class="form-control" type="file" name="images[]" id="images" multiple accept="image/*">
+                @error('images.*')
+                    {{ $message }}
+                @enderror
+                <button class="btn btn-primary" type="submit">Upload</button>
+            </div>
+
         </form>
-        <img src="{{ asset('storage/' . $image->path)  }}" alt="{{ $image->name }}">
+
+        @foreach ($images as $image)
+            <div>
+                <h3>
+                    {{ $image->name }}
+                </h3>
+                <form action="{{ route('images.destroy', ["imageName" => $image->base]) }}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <button class="btn btn-primary mb-2" type="submit">{{ __('Delete image') }}</button>
+                </form>
+                <img class="mb-4" src="{{ asset('storage/' . $image->path)  }}" alt="{{ $image->name }}">
+            </div>
+        @endforeach
+
     </div>
-@endforeach
+
+@endsection
