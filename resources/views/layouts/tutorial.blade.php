5e18bf4807f84fb4e38decf25a3a972d5465f128
Kristína Leitmanová <kristina.leitmanova@gmail.com>
2023-05-25 23:13:47 +0200

    PDF fix

--- resources/views/layouts/tutorial.blade.php
+++ resources/views/layouts/tutorial.blade.php
@@ -4,7 +4,7 @@
 
     <div class="content min-h-screen w-100">
 
-        <a href="{{ route('tutorial.download') }}" class="btn btn-outline-light w-100" target="_blank">{{ __('DOWNLOAD PDF') }}</a>
+        <a id="downloadPDF" href="{{ route('tutorial.download') }}" class="btn btn-outline-light w-100" target="_blank">{{ __('DOWNLOAD PDF') }}</a>
 
         @switch(auth()->user()->role->name)
             @case(App\Models\Role::$STUDENT)
@@ -18,4 +18,17 @@
         @endswitch
     </div>
 
+@endsection
+
+@section('scripts')
+    @parent
+
+    <script>
+        $("#downloadPDF").click((event) => {
+
+            event.preventDefault();
+
+            print();
+        });
+    </script>
 @endsection
