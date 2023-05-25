60b9486f3026d59914af875652fb6dfa6f2ebf60
Matúš Néma <matusnema@gmail.com>
2023-05-25 13:23:05 +0200

    Teacher language fix

--- resources/views/tutorial/teacher.blade.php
+++ resources/views/tutorial/teacher.blade.php
@@ -10,9 +10,8 @@
 <div class="card mt-4 mb-4">
     <div class="card-body">
 
-        <p> {{ __('Na podstránke Študenti, si môžete pozrieť všetkých študentov s ich ID, počtom vygenerovaných, odovzdaných príkladov a bodov. Túto tabuľku si možete stiehnuť ako CSV.') }} </p>
-        <p> {{ __('Študentov je možné triediť kliknutím na názov stĺpca.') }} </p>
-        <p> {{ __('On the Students subpage, you can view all students with their ID, the number of generated and submitted tasks. You can download this table as CSV.') }} </p>
+        <p> {{ __('On the Students subpage, you can view all students with their ID, the number of generated,  submitted tasks and points. You can download this table as CSV.') }} </p>
+        <p> {{ __('Students can be sorted by clicking on the column name.') }} </p>
         <img class="img-fluid shadow" src="{{ asset("img/tutorial/teacher/student_list.png") }}">
 
     </div>
