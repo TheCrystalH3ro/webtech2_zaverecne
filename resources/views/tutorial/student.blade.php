96da44b964906f98e2145f72b0c90109f65df9d9
Kristína Leitmanová <kristina.leitmanova@gmail.com>
2023-05-25 02:29:27 +0200

    Student tutorial

---
+++ resources/views/tutorial/student.blade.php
@@ -0,0 +1,41 @@
+<div class="card mt-4 mb-4">
+    <div class="card-body">
+
+        <p> {{ __('Po prihlásení sa ocitnete na domovskej stránke. V ľavej časti navigácie môžete prejsť na inú podstránku a v pravej zmeniť jazyk alebo sa odhlásiť.') }} </p>
+        <img class="img-fluid shadow" src="{{ asset("img/tutorial/student/homepage_empty.png") }}">
+
+    </div>
+</div>
+
+<div class="card mt-4 mb-4">
+    <div class="card-body">
+
+        <p> {{ __('Po kliknutí na generovať, si môžete vybrať sady úloh, ktoré vám priradil učiteľ.') }} </p>
+        <img class="img-fluid shadow" src="{{ asset("img/tutorial/student/task_generation_filled.png") }}">
+
+        <p class="mt-3"> {{ __('Ak už máte vygenerované všetky sady úloh, zobrazí sa vám táto prázdna stránka.') }} </p>
+        <img class="img-fluid shadow" src="{{ asset("img/tutorial/student/task_generation_empty.png") }}">
+
+    </div>
+</div>
+
+<div class="card mt-4 mb-4">
+    <div class="card-body">
+
+        <p> {{ __('Následne po generácií príkladov sa vám zobrazia na hlavnej stránke.') }} </p>
+        <img class="img-fluid shadow" src="{{ asset("img/tutorial/student/homepage_filled.png") }}">
+
+    </div>
+</div>
+
+<div class="card mt-4 mb-4">
+    <div class="card-body">
+
+        <p> {{ __('Po kliknutí na tlačítko vyriešiť sa vám zobrazí táto stránka.') }} </p>
+        <img class="img-fluid shadow" src="{{ asset("img/tutorial/student/task_view.png") }}">
+
+        <p class="mt-3"> {{ __('Nižšie sú vypísané inštrukcie pre používanie matematického editoru a miesto pre vyplnenie riešenia.') }} </p>
+        <img class="img-fluid shadow" src="{{ asset("img/tutorial/student/task_answer.png") }}">
+
+    </div>
+</div>
